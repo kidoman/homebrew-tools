3629148194c912886bb1cef45cb24b047a4f272b
Karan Misra <kidoman@gmail.com>
2014-03-30 21:40:29 +0530

    upgrade to v0.1.3

--- serve.rb
+++ serve.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Serve < Formula
   homepage "https://github.com/kidoman/serve"
-  url "https://github.com/kidoman/serve/archive/v0.1.2.tar.gz"
-  sha1 "fe280dbd8369f86df5d7f68c0327a1ea300e3b8d"
+  url "https://github.com/kidoman/serve/archive/v0.1.3.tar.gz"
+  sha1 "c7115d2212c740ee157ac86aa60e1ba27bdd1552"
 
   head "https://github.com/kidoman/serve.git"
 
